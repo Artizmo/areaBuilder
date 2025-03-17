925ec7a5b7176b5e0ed77ec96d3114d4f83ca5bb
Brian Selvaggio <brian.selvaggio@eddiebauer.com>
2025-03-17 09:01:44 -0500

    init merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,99 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect, useState } from "react";
+
+type Tile = {
+  x: number
+  y: number
+  room?: Room
+  connection?: Connector
+}
+
+type Room = {
+  id: number
+  title: string
+  n?: number
+  e?: number
+  s?: number
+  w?: number
+  u?: number
+  d?: number
+}
+
+type Connector = {
+  id: number
+  isVertical: boolean,
+  source: Tile,
+  destination: Tile,
+}
+
+const GRID_SIZE = 32;
+
+const tiles: Tile[] = [];
+for (let y = 1; y <= GRID_SIZE; y++) {
+  for (let x = 1; x <= GRID_SIZE; x++) {
+    tiles.push({ x, y });
+  }
+}
+
+function TileEditor({ tile }: { tile: Tile }) {
+  return (
+    <section className="text-[#9ca2ac]">
+      <div><span className="w-12 inline-block mb-2">tile:</span><span className="text-[#afbdd4] font-bold text-[18px]bg-[#171d27]">{tile.x}, {tile.y}</span></div>
+      <div><span className="w-12 inline-block mb-2">id:</span><input className="bg-[#171d27] rounded-[5px] border-[#303338] border-1" type="text" /></div>
+      <div><span className="w-12 inline-block mb-2">name:</span><input className="bg-[#171d27] rounded-[5px] border-[#303338] border-1" type="text" /></div>
+      <div><span className="w-12 inline-block mb-2">n:</span><input className="bg-[#171d27] rounded-[5px] border-[#303338] border-1" type="text" /></div>
+      <div><span className="w-12 inline-block mb-2">e:</span><input className="bg-[#171d27] rounded-[5px] border-[#303338] border-1" type="text" /></div>
+      <div><span className="w-12 inline-block mb-2">s:</span><input className="bg-[#171d27] rounded-[5px] border-[#303338] border-1" type="text" /></div>
+      <div><span className="w-12 inline-block mb-2">w:</span><input className="bg-[#171d27] rounded-[5px] border-[#303338] border-1" type="text" /></div>
+      <div><span className="w-12 inline-block mb-2">u:</span><input className="bg-[#171d27] rounded-[5px] border-[#303338] border-1" type="text" /></div>
+      <div><span className="w-12 inline-block mb-2">d:</span><input className="bg-[#171d27] rounded-[5px] border-[#303338] border-1" type="text" /></div>
+      {tile?.connection && (
+        <div>connection section</div>
+      )}
+      <div className="flex gap-x-4 mt-4 justify-end">
+        <button className="px-4 py-1 flex items-center justify-center border-[#61b882] border-1 rounded-[4]">clear</button>
+        <button className="px-4 py-1 flex items-center justify-center border-[#61b882] border-1 rounded-[4]">save</button>
+      </div>
+    </section>
+  )
+}
+
+function Tile({ onClick, tile }: { onClick: (tile: Tile) => void, tile: Tile }) {
+  // useEffect(() => {
+  //   console.log('bingo tile render')
+  // })
+
+  return (
+    <>
+      <div onClick={() => onClick(tile)} className="bg-[#171d27] w-[1rem] h-[1rem] hover:bg-[#454e5e]" />
+    </>    
+  )
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [map, setMap] = useState<Tile[]>([])
+  const [tile, setTile] = useState<Tile | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  // useEffect(() => {
+  //   console.log('bingo render')
+  // })
+
+  const handleTileClick = (tile: Tile) => {
+    // console.log('bingo tile', tile)
+    setTile(tile)
+  }
+
+  return (
+    <article className="flex justify-center items-center gap-x-4">
+      <div className="p-1 grid gap-1 grid-cols-[repeat(32,minmax(0,1rem))] border-1 border-solid border-[#303338]">
+        {tiles.map((tile, i) => (
+          <Tile key={i} tile={tile} onClick={handleTileClick} />
+        ))}
+      </div>
+      {tile && (
+        <TileEditor tile={tile} />
+      )}
+    </article>
   );
 }
